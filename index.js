ff9266d99e1403127ea23ca4f8c9f4214ef4630e
Hema Venkata <hvenkata@live.com>
2019-06-13 21:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  kittens.push(name)
-  return kittens
+  var newKittens = [name, ...kittens]
+  return newKittens
 }
 
 function prependKitten(name) {
-  kittens.unshift(name)
-  return kittens
+  var newKittens = [...kittens, name]
+  return newKittens
 }
 
 function removeLastKitten() {
